42396573bb7ffbcccd9f1c371d03332abed5eccd
zachlamont <zachlamont1@gmail.com>
2023-06-04 16:11:31 +1000

    remove video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import products from "../products";
+import backgroundImg from "../../assets/background.jpg";
 
 const Home = () => {
   return (
-    <div className="content">
-      <iframe
-  title="YouTube Video"
-  width="560"
-  height="315"
-  src="https://www.youtube.com/embed/_qAWGvIcFoc"
-  frameBorder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowFullScreen
-></iframe>
+    <div className="content" style={{ backgroundImage: `url(${backgroundImg})` }}>
       <h1>
-        Music at Your Fingertips. <br></br> Get it Fast.{" "}
+        Music at Your Fingertips. <br /> Get it Fast.
       </h1>
     </div>
   );
