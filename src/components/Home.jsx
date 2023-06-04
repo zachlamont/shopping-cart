622a7bbfcc27c85aa46a686ae27ccf8e180ade67
zachlamont <zachlamont1@gmail.com>
2023-06-04 16:18:38 +1000

    tweak background image path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import products from "../products";
-import backgroundImg from "../../assets/background.jpg";
+import backgroundImg from "../assets/background.jpg";
 
 const Home = () => {
   return (
-    <div className="content" style={{ backgroundImage: `url(${backgroundImg})` }}>
+    <div
+      className="content"
+      style={{ backgroundImage: `url(${backgroundImg})` }}
+    >
       <h1>
         Music at Your Fingertips. <br /> Get it Fast.
       </h1>
