290506f976e186a9de083f3c599c7b7d13d70549
zachlamont <zachlamont1@gmail.com>
2023-06-04 12:40:23 +1000

    Add styles to Cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Cart = ({ cartItems, removeFromCart, updateQuantity }) => {
   return (
-    <div>
+    <div className="cart-container">
       {cartItems.map((item) => (
-        <div key={item.product.name}>
-          <img src={item.product.image} alt={item.product.name} />
-          <h3>{item.product.name}</h3>
-          <p>${item.product.price}</p>
-          <input
+        <div className='cart-item'key={item.product.name}>
+          <img className='cart-image' src={item.product.image} alt={item.product.name} />
+          <h4 className="cart-item-name">{item.product.name}</h4>
+          <p className='cart-item-price'>${item.product.price}</p>
+          <input className="cart-item-quantity"
             type="number"
             value={item.quantity}
             onChange={(e) => updateQuantity(item.product, e.target.value)}
           />
-          <button onClick={() => removeFromCart(item.product)}>Delete</button>
+          <button className='cart-item-button' onClick={() => removeFromCart(item.product)}>Delete</button>
         </div>
       ))}
     </div>
