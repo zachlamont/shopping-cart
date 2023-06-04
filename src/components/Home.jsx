b45d3de654f13a7085b0b877b900809dc0fa4156
zachlamont <zachlamont1@gmail.com>
2023-06-04 15:15:11 +1000

    change video path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,10 @@ const Home = () => {
   return (
     <div className="content">
       <video autoPlay loop muted>
-        <source src={"https://github.com/zachlamont/shopping-cart/blob/3182efa8cc8b29504990c8e269c32fa3ad0dfc40/src/assets/vid1.mp4"} type="video/mp4" />
+        <source
+          src={"https://www.youtube.com/watch?v=_qAWGvIcFoc"}
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
       <h1>
